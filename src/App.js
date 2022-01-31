f2384c4f1ea11fe851ca6b3a5c8c549a9eb9aae0
Schuyler <skym1n0r91@gmail.com>
2022-01-31 16:53:35 -0600

    login and signup template created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login/Login';
+import SignUp from './components/SignUp/SignUp';
+
+import { Link } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Welcome To The Project</h1>
+			<Link to='/login'>Login</Link>
+			<Login path="/login"/>
+			<SignUp />
+		
+
 		</div>
+
+		
+		
+		
+		
 	);
 }
 
---
+++ src/components/Login/Login.js
@@ -0,0 +1,34 @@
+import React, {useState} from "react";
+import { Button, Form, Input, Label } from "reactstrap";
+
+
+const Login = () => {
+    const [login, setLogin] = useState({
+        username: "",
+        password: "",
+    })
+
+    return (
+        <Form>
+            <h1>Login</h1>
+            <Label>USERNAME
+                <Input 
+                name="username"
+                type="text"
+             
+                />
+            </Label>
+            <Label>
+                PASSWORD
+                <Input 
+                name="password"
+                type="password"
+                />
+            </Label>
+            <Button>Login</Button>
+
+        </Form>
+    )
+};
+
+export default Login;
