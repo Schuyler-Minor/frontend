f75fdef040705c58e2775ad7d89fc7d77f4cb1d0
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-02-01 02:17:18 -0500

    Added other components, as well as setup the mock API

--- src/components/MyClasses/MyClasses.js
+++ src/components/MyClasses/MyClasses.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import Class from '../Class/Class';
 import './index.css';
 
+const initialState = [];
+
 const MyClasses = () => {
+	const [classes, setClasses] = useState(initialState);
+	useEffect(() => {
+		axios
+			.get('https://61f8c846783c1d0017c44770.mockapi.io/api/v1/class')
+			.then((res) => {
+				setClasses(res.data);
+			})
+			.catch((err) => console.log(err));
+	}, []);
+
 	return (
 		<div>
 			<h1>My Classes</h1>
-			<ul>
-				<li>Class 1</li>
-				<li>Class 2</li>
-				<li>Class 3</li>
-			</ul>
+
+			{classes.map((item, idx) => (
+				<Class key={idx} class={item} />
+			))}
 		</div>
 	);
 };
