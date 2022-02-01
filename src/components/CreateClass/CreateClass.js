869a3818e607b162b40adfe9c953261a011f70ad
Schuyler <skym1n0r91@gmail.com>
2022-02-01 15:07:47 -0600

    labels and inputs

--- src/components/CreateClass/CreateClass.js
+++ src/components/CreateClass/CreateClass.js
@@ -10,8 +10,42 @@ const CreateClass = () => {
             <Label>Class Name:
                 <Input 
                     name="class_name"
+                    type="text"
                 />
             </Label>
+            <br />
+
+            <Label>Class Type
+                <Input 
+                 />
+            </Label>
+                <br />
+            <Label>Start Time
+                <Input />
+            </Label>
+                <br />
+            <Label>Duration
+                <Input />
+            </Label>
+                <br />
+            <Label>Intensity Level
+                <Input />
+            </Label>
+                <br/>
+            <Label>Location
+                <Input />
+            </Label>
+                <br/>
+            <Label>Current number of registered attendees
+                <Input />
+            </Label>
+                <br />
+            <Label>Max Class Size
+                <Input />
+            </Label>
+                <br />
+                <Button>Create Class</Button>
+
         </Form>
     )
 }
