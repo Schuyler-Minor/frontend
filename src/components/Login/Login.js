e8e05dfb7329a86b21ae863dcb57e35d2147397e
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-02-01 02:36:22 -0500

    re: testing reployment.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,34 +1,26 @@
-import React, {useState} from "react";
-import { Button, Form, Input, Label } from "reactstrap";
-
+import React /*, { useState }*/ from 'react';
+import { Button, Form, Input, Label } from 'reactstrap';
 
 const Login = () => {
-    const [login, setLogin] = useState({
-        username: "",
-        password: "",
-    })
+	// const [login, setLogin] = useState({
+	//     username: "",
+	//     password: "",
+	// })
 
-    return (
-        <Form>
-            <h1>Login</h1>
-            <Label>USERNAME
-                <Input 
-                name="username"
-                type="text"
-             
-                />
-            </Label>
-            <Label>
-                PASSWORD
-                <Input 
-                name="password"
-                type="password"
-                />
-            </Label>
-            <Button>Login</Button>
-
-        </Form>
-    )
+	return (
+		<Form>
+			<h1>Login</h1>
+			<Label>
+				USERNAME
+				<Input name="username" type="text" />
+			</Label>
+			<Label>
+				PASSWORD
+				<Input name="password" type="password" />
+			</Label>
+			<Button>Login</Button>
+		</Form>
+	);
 };
 
 export default Login;
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,51 +1,39 @@
-import React, { useState } from "react";
-import { Button, Form, Input, Label } from "reactstrap";
+import React /*, { useState }*/ from 'react';
+import { Button, Form, Input, Label } from 'reactstrap';
 
 const SignUp = () => {
-    const [signUp, setSignUp] = useState({
-        "username": "",
-        "password": "",
-        "email": "",
-      })
+	// const [signUp, setSignUp] = useState({
+	//     username: "",
+	//     password: "",
+	//     email: "",
+	//   })
 
-    return (
-        <Form>
-            <h3>Sign Up</h3>
-      <Label>
-        USERNAME
-        <Input
-          name="username"
-          type="text"
-        
-        />
-      </Label>
+	return (
+		<Form>
+			<h3>Sign Up</h3>
+			<Label>
+				USERNAME
+				<Input name="username" type="text" />
+			</Label>
 
-      <Label>
-        EMAIL
-        <Input
-          name="email"
-          type="email"
-      
-        />
-      </Label>
-      <Label>
-      PASSWORD
-        <Input
-          name="password"
-          type="password"
-        
-        /> 
-      </Label>
-      <Label>Are you a client or Instructor?
-      <select name="role_type">
-        <option>Client</option>
-        <option>Instructor</option>
-      </select>
-      </Label>
-        <Button>Sign Up!!!</Button>
-
-        </Form>
-    )
-}
+			<Label>
+				EMAIL
+				<Input name="email" type="email" />
+			</Label>
+			<Label>
+				PASSWORD
+				<Input name="password" type="password" />
+			</Label>
+			<Label>
+				Are you a client or Instructor?
+				<select name="role_type">
+					<option>Client</option>
+					<option>Instructor</option>
+				</select>
+			</Label>
+			<Button>Sign Up!!!</Button>
+		</Form>
+	);
+};
 
 export default SignUp;
