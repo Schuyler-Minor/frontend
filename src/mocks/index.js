4fc07473e12b8aba648192f1c5df897c58fb6fc3
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-02-01 02:22:54 -0500

    Added instructions for the Mock API in the mocks/index.js folder.

---
+++ src/mocks/index.js
@@ -0,0 +1,15 @@
+// CLASSES API ENDPOINT
+// https://61f8c846783c1d0017c44770.mockapi.io/api/v1/class
+
+// USERS API ENDPOINT
+// https://61f8c846783c1d0017c44770.mockapi.io/api/v1/users
+
+// To make PUT, DELETE Requests, use below
+
+// CLASSES API ENDPOINT
+// https://61f8c846783c1d0017c44770.mockapi.io/api/v1/class/:id
+
+// USERS API ENDPOINT
+// https://61f8c846783c1d0017c44770.mockapi.io/api/v1/users/:id
+
+// Remember to pass in the correct id in place of {:id}
